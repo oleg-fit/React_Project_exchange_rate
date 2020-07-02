db99166d5d26666ec1bd27122638dd02e48b5c0b
oleg-fit <oleg26-spb@rambler.ru>
2020-07-02 19:12:40 +0300

    Разработали блок с калькулятором

--- src/App.js
+++ src/App.js
@@ -18,37 +18,6 @@ class App extends React.Component {
                 <div className="container">
                     <main>
                         <Rate />
-                        <h3> Калькулятор обмена</h3>
-                        <div className="block">
-                            <div>Я хочу</div>
-                            <div>
-                                <label>
-                                    <input type="radio" name="radio" defaultValue="0" /> купить
-                                </label>
-                            </div>
-                            <div>
-                                <label>
-                                    <input type="radio" name="radio" defaultValue="1" /> продать
-                                </label>
-                            </div>
-                            <div>
-                                <input type="number" defaultValue="150" />
-                                <select name="" id="">
-                                    <option value="USD">USD</option>
-                                    <option value="RUB">RUB</option>
-                                    <option value="EUR">EUR</option>
-                                </select>
-                            </div>
-                            <div>
-                                <h4>Результат</h4>
-                                <ul className="calc-res">
-                                    <li>EUR 150</li>
-                                    <li>EUR 150</li>
-                                    <li>EUR 150</li>
-                                    <li>EUR 150</li>
-                                </ul>
-                            </div>
-                        </div>
                     </main>
                 </div>
 
--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Rate.css';
+import Calc from '../Calc/Calc';
 
 class Rate extends React.Component {
     constructor(props) {
@@ -51,6 +52,7 @@ class Rate extends React.Component {
                         )
                     )};
                 </div>
+                <Calc rate={this.state.currencyRate} />
             </div>
         );
     }
